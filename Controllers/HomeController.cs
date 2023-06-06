ec2447eed8c3df5904929b99aaa7161051c3293d
admin <admin@DESKTOP-8GOT5CO>
2023-06-07 03:30:08 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiSelectChechboxApp.Models;
+using System.Diagnostics;
+
+namespace MultiSelectChechboxApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(List<Employee> EmpModel)
+        {
+            //Save List employee Record To db.
+            return Json(true);
+        }
+        
+
+        public IActionResult Privacy()  
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/Custom/Employees.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    $("#btnGet").click(function () {
+        var empArr = [];
+        $(".multiSelect").each(function (i, item) {
+            if ($(item).is(":checked")) {
+                let name = $(item).closest('tr').find('.empName').val();
+                let id = $(item).closest('tr').find('.id').val();
+                let obj = {};
+                obj.Id = id;
+                obj.Name = name;
+                empArr.push(obj);
+            }
+        })
+        if (empArr.length > 0) {
+            AddEmp(empArr);
+        }
+    });
+})
+
+function AddEmp(empRecords) {
+    $.ajax({
+        type: 'POST',
+        url: "/Home/AddEmployee",
+        dataType: "json",
+        data: { EmpModel: empRecords },
+        success: function (res) {
+            if (res) {
+                alert("Record Added Successfully !..")
+            }
+        }
+    })
+}
+
+
+
+
+
+
